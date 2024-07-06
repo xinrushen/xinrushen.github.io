cfd557db02dac4a7b7874882c347848551651891
Xinru Shen <55038606+xinrushen@users.noreply.github.com>
2024-07-06 07:33:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ window.addEventListener('scroll', () => {
   }
 });
 
+// Smooth scrolling when clicking navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
