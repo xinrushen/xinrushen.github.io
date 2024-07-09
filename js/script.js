f5b1446d9b1fc6f0594cd54a0ee0a77388abe755
Xinru Shen <55038606+xinrushen@users.noreply.github.com>
2024-07-09 08:11:06 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var content = document.querySelector('.content');
+    
+    if (sidebar.style.left === '-250px') {
+        sidebar.style.left = '0';
+        content.style.marginLeft = '250px';
+    } else {
+        sidebar.style.left = '-250px';
+        content.style.marginLeft = '0';
+    }
+}
 
