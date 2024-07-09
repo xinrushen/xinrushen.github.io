d3e5d7df0e1c7ca773eb71b18017b1fed3757656
Xinru Shen <55038606+xinrushen@users.noreply.github.com>
2024-07-09 17:41:00 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function(e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+        });
+    });
+});
