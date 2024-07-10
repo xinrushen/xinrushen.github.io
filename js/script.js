fc87b72e7d7e40418a7f50dac88ba34c025dd599
Xinru Shen <55038606+xinrushen@users.noreply.github.com>
2024-07-10 07:43:23 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+ScrollTrigger.create({
+  animation: gsap.from(".logo", {
+    y: "50vh",
+    scale: 6,
+    yPercent: -50,
+  }),
+  scrub: true,
+  trigger: ".content",
+  start: "top bottom",
+  endTrigger: ".content",
+  end: "top center",
+});
