b9fc6fb8df0802f432a4e485c51efeded0e0bfa0
Xinru Shen <55038606+xinrushen@users.noreply.github.com>
2024-10-29 14:07:18 +0000

    dsfsadfdfsdf

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+ScrollTrigger.create({
+  animation: gsap.from(".logo", {
+    y: "50vh",
+    scale: 6,
+    yPercent: -50,
+  }),
+  scrub: true,
+  trigger: ".content",
+  start: "top bottom",
+  endTrigger: ".content",
+  end: "top center",
+});
+
 function openNav() {
   document.getElementById("mySidepanel").style.width = "250px";
 }
@@ -13,17 +26,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
                     behavior: 'smooth'
         });
     });
-});
-
-ScrollTrigger.create({
-  animation: gsap.from(".logo", {
-    y: "50vh",
-    scale: 6,
-    yPercent: -50,
-  }),
-  scrub: true,
-  trigger: ".content",
-  start: "top bottom",
-  endTrigger: ".content",
-  end: "top center",
 });
