0fc0331e15bdf3d6e6be57bb7792121f18ca6483
Xinru Shen <55038606+xinrushen@users.noreply.github.com>
2024-11-01 20:39:49 +0000

    dark mod

--- js/script.js
+++ js/script.js
@@ -45,3 +45,19 @@ const sections = document.querySelectorAll('.section');
             scrollToSection(currentSection - 1);
         }
     });
+
+const toggleButton = document.getElementById('darkMode');
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+}
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
